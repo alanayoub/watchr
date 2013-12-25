43626fb7fda61bade63ca10e07c13a2a47a590ae
Alan Ayoub <alanayoub@gmail.com>
2013-12-25 10:32:26 +0000

    persisting login attempts

--- index.js
+++ index.js
@@ -79,11 +79,23 @@ app.get('/', function (req, res) {
     res.render('index');
 });
 
-app.post('/api/login', passport.authenticate('local'), function (req, res) {
-    console.log('trying to login');
-    if (req.user) {
-        res.send('You are logged in');
-    }
+app.post('/api/login', function (req, res, next) {
+    passport.authenticate('local', function (error, user, info) {
+        var query = 'INSERT INTO watchr.login_attempt (username, ip, success) VALUES (?, ?, ?)',
+            data = [req.body.username, req.connection.remoteAddress, !!user ? 1 : 0];
+        if (error) return next(error);
+        pool.getConnection(function (error, connection) {
+            if (error) throw error;
+            connection.query(query, data, function (error, results) {
+                connection.release();
+            });
+        });
+        if (!user) return res.send('Incorrect credentials: You are NOT logged in');
+        req.logIn(user, function (error) {
+            if (error) return next(error);
+            return res.send('You are logged in');
+        });
+    })(req, res, next);
 });
 
 app.post('/api/logout', function (req, res) {
