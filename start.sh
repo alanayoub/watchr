1e758003e8fe42cafdac71123fa0642daf88e69f
Alan Ayoub <alanayoub@gmail.com>
2014-01-12 19:34:09 +0000

    add tmux start script

---
+++ start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+tmux -2 new -d -n 'scratch' -s watchr
+tmux neww -t watchr:1 -n 'git'
+tmux neww -t watchr:2 -n 'grunt' 'grunt dev'
+tmux neww -t watchr:3 -n 'node' 'npm start'
+tmux neww -t watchr:4 -n 'redis' 'redis-cli monitor'
+tmux neww -t watchr:5 -n 'splunk' -c /opt/splunk/bin './splunk start'
+tmux neww -t watchr:6 -n 'mysql'
+tmux neww -t watchr:7 -n 'ws' -c /opt/WebStorm-129.664/bin/ './webstorm.sh'
+tmux send-keys -t watchr:7 '\n' C-m
+tmux select-window -t watchr:3
+tmux -2 attach-session -t watchr
