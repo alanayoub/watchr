9e9805ff0cec14c80e394d08345529c5eefe0b8f
Alan Ayoub <alanayoub@gmail.com>
2014-01-19 11:06:02 +0000

    testing for stale jobs

--- services/kue/kue_jobs.js
+++ services/kue/kue_jobs.js
@@ -26,22 +26,39 @@ module.exports = function () {
     var createjobs = function (tasks) {
         logger.info('kue.js: Create jobs');
         tasks.forEach(function (val, idx, arr) {
-            jobs.create('scrape', {title: 'Scrape: ' + val.url, selector: val.css, url: val.url}).save();
+            var options = {title: 'Scrape: ' + val.url, selector: val.css, url: val.url};
+            jobs.create('scrape', options).attempts(5).save();
         });
     };
-    var kuemanager = function () {
-        jobs.activeCount(function (error, result) {
-            if (error) logger.error('kue.js: jobs.activeCount: %j', error);
-            logger.info('kue.js: jobs.activeCount: %d', result);
-            if (result === 0) gettasks().then(function (result) {
-                if (result.error) logger.error('kue.js: Error getting tasks: %j', result.error);
-                if (result.data.length) createjobs(result.data);
-            });
-        });
+    var Kuemanager = function () {
+        var last_active, lastupdated = +new Date, stale_milliseconds = 1000 * 30;
+        return {
+            update: function () {
+                jobs.activeCount(function (error, result) {
+                    if (error) logger.error('kue.js: jobs.activeCount: %j', error);
+                    logger.info('kue.js: jobs.activeCount: %d', result);
+                    if (kuemanager.is_stale(result)) {
+                        logger.info('Kue is stale, need to clean this shizzle up yo');
+                    }
+                    if (result === 0) gettasks().then(function (result) {
+                        if (result.error) logger.error('kue.js: Error getting tasks: %j', result.error);
+                        if (result.data.length) createjobs(result.data);
+                        lastupdated = +new Date;
+                    });
+                });
+            },
+            is_stale: function (active) {
+                var stale = (last_active === active && +new Date - lastupdated > stale_milliseconds) ? true : false;
+                last_active = active;
+                return stale;
+            }
+        }
     };
+    var kuemanager = new Kuemanager();
+    kuemanager.update();
     jobs.on('complete', function () {
         logger.info('Job complete');
-        kuemanager();
+        kuemanager.update();
     })
     .on('failed', function () {
         logger.warn('Job failed');
@@ -50,6 +67,5 @@ module.exports = function () {
         logger.info('Job progress');
         //process.stdout.write('\r  job #' + job.id + ' ' + progress + '% complete');
     });
-    kuemanager();
-    setInterval(kuemanager, config.get('app:kue:update:interval'));
+    setInterval(kuemanager.update, config.get('app:kue:update:interval'));
 };
