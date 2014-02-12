9b6c3b04acbd9a7aa810bc853a7f53e8caca6089
Alan Ayoub <alanayoub@gmail.com>
2014-02-12 16:54:56 +0000

    removing display of result under form

--- public/views/search/view.js
+++ public/views/search/view.js
@@ -14,7 +14,6 @@ define(['jquery', 'socket', 'backbone'], function ($, socket) {
                     return (acc[val.name] = val.value) && acc;
                 }, {});
                 watchr.rest.search.post({data: data}).then(function (result) {
-                    view.$el.find('.w-answer').html(+new Date + ': ' + result);
                     socket.emit('tasks');
                 });
                 event.preventDefault();
