e178aadf9d49d8118a177b784efe222dd7d60f74
Alan Ayoub <alanayoub@gmail.com>
2014-03-27 18:37:13 +0000

    Adding AWS vagrant setup

--- public/services/socket.js
+++ public/services/socket.js
@@ -1,5 +1,5 @@
 'use strict';
 define(['socket'], function () {
-    var socket = io.connect('http://localhost:3000/');
+    var socket = io.connect('http://54.85.189.132:3000/');
     return socket;
 });
