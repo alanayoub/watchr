6dc64a955e7538877eaaa83d3f13c3db7324fe40
Alan Ayoub <alanayoub@gmail.com>
2014-05-24 09:43:49 +0100

    Sending tasks to chrome extention via sockets

--- browser_extensions/chrome/background.js
+++ browser_extensions/chrome/background.js
@@ -1,3 +1,12 @@
+var socket = io.connect('http://localhost:3000');
+socket.on('connect', function () {
+    console.log('connected');
+    socket.emit('chromeGetTasks', {});
+    socket.on('chromeTasks', function (data) {
+        console.log('updatearino', data);
+    });
+});
+
 var frame = document.getElementsByTagName('iframe')[0],
     selector = 'h2:first a';
 frame.src = "http://alanayoub.com/";
