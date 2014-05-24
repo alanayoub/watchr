4374f8589c9e8658ba63ab9b3d9c368925cc2676
Alan Ayoub <alanayoub@gmail.com>
2014-05-24 17:21:09 +0100

    scraping within chrome extention wip

--- browser_extensions/chrome/background.js
+++ browser_extensions/chrome/background.js
@@ -1,16 +1,61 @@
-var socket = io.connect('http://localhost:3000');
+var socket = io.connect('http://localhost:3000'),
+    frame = document.getElementsByTagName('iframe')[0],
+    tasks, task, source;
+
 socket.on('connect', function () {
-    console.log('connected');
     socket.emit('chromeGetTasks', {});
     socket.on('chromeTasks', function (data) {
         console.log('updatearino', data);
+        source = null;
+        task = data[0];
+        task.css = '.s9TitleText:first';
+        task.url = 'http://www.amazon.co.uk';
+        tryIframe()
+            .done(extractResult)
+            .fail(tryAsync()
+                .done(extractResult)
+                .fail(function () {
+                    console.log('failed both');
+                })
+            );
     });
 });
 
-var frame = document.getElementsByTagName('iframe')[0],
-    selector = 'h2:first a';
-frame.src = "http://alanayoub.com/";
+var tryIframe = function () {
+    var $deferred = $.Deferred();
+    frame.src = task.url;
+    setTimeout(function () {
+        if (source === null) {
+            console.log('iframe didnt work');
+            $deferred.reject();
+        }
+        else $deferred.resolve();
+    }, 2000);
+    return $deferred.promise();
+}
+
+var tryAsync = function (data) {
+    var $deferred = $.Deferred();
+    $.ajax({
+        url: task.url,
+        type: 'GET'
+    }).done(function (result) {
+        console.log('Ajax worked');
+        source = result;
+        $deferred.resolve();
+    }).fail(function (error) {
+        console.log('Ajax didn\'t work', error);
+        source = result;
+        $deferred.reject();
+    });
+    return $deferred.promise();
+};
+
+var extractResult = function () {
+    console.log('result', $(source).find(task.css).text());
+}
 
 chrome.extension.onMessage.addListener(function (request, sender, sendResponse) {
-    console.log('background result', $(request.result).find(selector).text());
+    source = request.result;
+    console.log('iframe did work');
 });
