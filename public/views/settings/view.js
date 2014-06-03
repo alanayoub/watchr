c216338dacbf90961f8e75c5e33c19567ec6272c
Alan Ayoub <alanayoub@gmail.com>
2014-06-03 21:47:25 +0100

    Expanding settings menu on click, adding fade icon

--- public/views/settings/view.js
+++ public/views/settings/view.js
@@ -5,6 +5,16 @@ define([
 ], function ($) {
     return Backbone.View.extend({
         events: {
+            'click': function (event) {
+                event.stopPropagation();
+                var $settings = this.$el.find('.W-settings');
+                if ($settings.hasClass('w-open')) return;
+                $settings.addClass('w-open');
+                $(document).on('click.close.settings', function () {
+                    $settings.removeClass('w-open');
+                    $(document).off('click.close.settings');
+                });
+            }
         },
         initialize: function () {
             var view = this;
@@ -14,7 +24,9 @@ define([
         render: function () {
             var view = this;
             view.$el.html(view.template(view.model));
-            view.$el.find('.svg').inlinesvg();
+            view.$el.find('.svg').each(function (idx, val) { // TODO: allow inlinesvg to take array
+                $(val).inlinesvg();
+            });
         }
     });
 });
