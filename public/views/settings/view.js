36396a9257c0d2eed4f909cb9f0ae4082b35e49e
Alan Ayoub <alanayoub@gmail.com>
2014-06-14 12:19:12 +0100

    Enable test button only if url or css fields change

--- public/views/settings/view.js
+++ public/views/settings/view.js
@@ -22,25 +22,38 @@ define([
             view.render();
         },
         render: function () {
-            var view = this, socket = watchr.socket;
+            var view = this,
+                socket = watchr.socket,
+                $save, $test, $form;
             view.$el.html(view.template(view.model.toJSON()));
-            view.$el.$save = view.$el.find('input[value=Save]');
-            view.$el.$form = view.$el.find('form'); 
             view.$el.find('.svg').each(function (idx, val) { // TODO: allow inlinesvg to take array
                 $(val).inlinesvg();
             });
+            $save = view.$el.find('input[name=save]');
+            $test = view.$el.find('input[name=test]');
+            $form = view.$el.find('form');
             view.$el.on('change keyup', 'input, select, textarea', function () {
-                var $submit = view.$el.$save;
-                if (view.$el.$form.serialize() !== view.$el.savedFormString) $submit.removeAttr('disabled');
-                else $submit.attr('disabled', 'disabled');
+                var oldModel = view.model,
+                    newModel = $form.serializeArrayFlat();
+                // enable test if url or css fields change
+                oldModel.get('url') !== newModel.url || oldModel.get('css') !== newModel.css
+                    ? $test.removeAttr('disabled')
+                    : $test.attr('disabled', 'disabled');
+                // enable submit if any field changes
+                $form.serialize() !== view.$el.savedFormString
+                    ? $save.removeAttr('disabled')
+                    : $save.attr('disabled', 'disabled')
             });
             view.$el.savedFormString = view.$el.find('form').serialize();
-            view.$el.find('form').off('submit.form').on('submit.form', function (event) {
+            view.$el.find('form [type=submit]').off('click.form').on('click.form', function (event) {
                 event.preventDefault();
-                var $form = view.$el.$form;
-                view.$el.$save.attr('disabled', 'disabled');
-                view.model.set($form.serializeArrayFlat());
-                socket.emit('cli:settings:save', view.model.toJSON());
+                if ($(this).attr('name') === 'test') {
+                }
+                else {
+                    $save.attr('disabled', 'disabled');
+                    view.model.set($form.serializeArrayFlat());
+                    socket.emit('cli:settings:save', view.model.toJSON());
+                }
             });
         }
     });
