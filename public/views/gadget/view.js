ec573c7ecca26ba945ff879a2fdae3a85d455d32
Alan Ayoub <alanayoub@gmail.com>
2014-06-29 19:16:59 +0100

    positioning plot label properly

--- public/views/gadget/view.js
+++ public/views/gadget/view.js
@@ -40,7 +40,7 @@ define([
             view.$el.find('.w-format').append(view.formatView.el);
             view.$el.find('.JS-settings').append(view.settingsView.el);
             if (result.format === 'Number') {
-                $.plot($(".w-flot"), [result.set], {
+                $.plot($('.w-flot'), [result.set], {
 //                yaxis: { min: 0 },
                     colors: ['#30A0EB'],
                     legend: {
@@ -68,10 +68,28 @@ define([
                     grid: {
                         show: true,
                         color: '#F9F9F9',
+                        hoverable: true,
                         backgroundColor: null,
                         borderWidth: 2
                     }
                 });
+                view.$el.find('.w-flot').on('plothover', function (event, pos, item) {
+                    if (item) {
+                        var x = item.datapoint[0].toFixed(2),
+                            y = item.datapoint[1].toFixed(2),
+                            offset = $(this).closest('.W-gadget-list-container').offset();
+                console.log(JSON.stringify(offset));
+                console.log(item.pageY, item.pageX);
+                        $('.w-tooltip').html(y)
+                            .css({top: (item.pageY - offset.top) + 5, left: (item.pageX - offset.left) + 5})
+                            .fadeIn(200);
+                    } else {
+                        $('.w-tooltip').hide();
+                    } 
+
+                });
+
+
             }
             if (result.format === 'String') {
 
